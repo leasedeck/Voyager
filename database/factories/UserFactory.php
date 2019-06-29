55d5970f3e99831183e9c49c1c2884c8dce1f81d
Tim Joosten <Tjoosten3@gmail.com>
2019-06-29 03:35:43 +0200

    [SEED]: change password to "password" -> Users
    [SIGNED OFF]: Tim Joosten <topairy@gmail.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'achternaam' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$JmVqy14i6l9lqxeCOl.BcesUNwgG9mf0sEXFH0cS44PUkAvpDCic6', // password
         'remember_token' => str_random(10),
     ];
 });
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -22,7 +22,7 @@ class UserTableSeeder extends Seeder
         collect($this->organisationMembers())->each(function (array $name): void {
             [$firstName, $lastName] = $name;
 
-            $data = ['voornaam' => $name[0], 'achternaam' => $name[1], 'email' => strtolower($name[0]) . '@activisme.be', 'password' => 'secret'];
+            $data = ['voornaam' => $name[0], 'achternaam' => $name[1], 'email' => strtolower($name[0]) . '@activisme.be', 'password' => 'password'];
             $user = $this->createBackUser($data);
 
             if ($this->isInWebmasterArray($user->email)) {
