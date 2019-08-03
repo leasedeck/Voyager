5a3d8492548192d8f2416ad7cb9d8ef3608f6b9f
Tim Joosten <Tjoosten3@gmail.com>
2019-08-03 18:56:58 +0200

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,69 @@
     </div>
 
     <div class="container-fluid pb-3">
+        <div class="row"> {{-- Widgets --}}
+            <div class="col-3"> {{-- lokalen widget --}}
+                <div class="card border-0 shadow-sm mb-4 p-2">
+                    <div class="d-flex align-items-center">
+                        <span class="stamp stamp-md shadow-sm bg-brown mr-3">
+                            <i class="fe fe-home"></i>
+                        </span>
 
+                        <div>
+                            <h5 class="m-0">0 <small>lokalen</small></h5>
+                            <small class="text-muted"></small>
+                        </div>
+                    </div>
+                </div> {{-- /// END lokalen widget --}}
+            </div> {{-- /// END widgets --}}
+
+            <div class="col-3">
+                <div class="card border-0 shadow-sm mb-4 p-2">
+                    <div class="d-flex align-items-center">
+                        <span class="stamp stamp-md shadow-sm bg-brown mr-3">
+                            <i class="fe fe-calendar"></i>
+                        </span>
+
+                        <div>
+                            <h5 class="m-0">0 <small>Verhuringen</small></h5>
+                            <small class="text-muted"></small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card border-0 shadow-sm mb-4 p-2"> {{-- Huurders widget --}}
+                    <div class="d-flex align-items-center">
+                        <span class="stamp stamp-md shadow-sm bg-brown mr-3">
+                            <i class="fe fe-users"></i>
+                        </span>
+
+                        <div>
+                            <h5 class="m-0">0 <small>Huurders</small></h5>
+                            <small class="text-muted"></small>
+                        </div>
+                    </div>
+                </div> {{-- /// ENd huurder widget --}}
+            </div>
+
+            <div class="col-3">
+                <div class="card border-0 shadow-sm mb-4 p-2">
+                    <div class="d-flex align-items-center">
+                        <span class="stamp stamp-md shadow-sm bg-brown mr-3">
+                            <i class="fe fe-alert-triangle"></i>
+                        </span>
+
+                        <div>
+                            <h5 class="m-0">0 <small>werkpunten</small></h5>
+                            <small class="text-muted"></small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> {{-- /// END widgets --}}
+    </div>
+
+    <div class="container-fluid pb-2">
     </div>
 @endsection
