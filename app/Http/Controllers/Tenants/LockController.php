4e1e73e00f25aeb9c7b3f92c1a985a7c61e42869
Tim Joosten <Tjoosten3@gmail.com>
2019-08-08 13:04:41 +0200

    Implementatie docblocks

--- app/Http/Controllers/Tenants/LockController.php
+++ app/Http/Controllers/Tenants/LockController.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Controller;
  * @todo Complete validator
  * @todo Register routes
  * @todo Write controller logic
+ * @todo Extends Tenant model for supporting bans.
  *
  * @package App\Http\Controllers\Tenants
  */
@@ -27,25 +28,53 @@ class LockController extends Controller
     public function __construct()
     {
         $this->middleware(['auth', '2fa', 'portal:application', 'forbid-banned-user']);
+
+        $this->middleware('can:lock,tenant')->only(['create', 'store']);
+        $this->middleware('can:unlock,tenant')->only('delete', 'update');
     }
 
+    /**
+     * Methode om de deactivatie weergave in de applicatie.
+     *
+     * @param  Tenant $tenant De databank entiteit van de huurder.
+     * @return Renderable
+     */
     public function create(Tenant $tenant): Renderable
     {
         // TODO
     }
 
+    /**
+     * Methode om de blokkade van de huurder op te slaan in de databank.
+     *
+     * @param  LockFormRequest  $request De requst instance die e validatie afhandeld en request data bijhoud
+     * @param  Tenant           $tenant  De databank entiteit van de huurder.
+     * @return RedirectResponse
+     */
     public function store(LockFormRequest $request, Tenant $tenant): RedirectResponse
     {
-        $this->authorize('lock', $tenant);
+        // TODO
     }
 
+    /**
+     * Methode voor de blokkerings weergave van de huurder.
+     *
+     * @param  Tenant $tenant De databank entiteit van de huurder
+     * @return Renderable
+     */
     public function delete(Tenant $tenant): Renderable
     {
         // TODO
     }
 
+    /**
+     * Methode om de blokkering van de huurder op te slaan in de databank.
+     *
+     * @param  Tenant $tenant De databnk entiteit van de huurder
+     * @return RedirectResponse
+     */
     public function update(Tenant $tenant): RedirectResponse
     {
-        $this->authorize('unlock', $tenant);
+        // TODO
     }
 }
