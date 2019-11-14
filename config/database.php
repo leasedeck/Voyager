94d4f6a752331f9de6b5f8e2f7b86593ca021e8d
Tim Joosten <Tjoosten3@gmail.com>
2019-11-14 08:02:42 +0100

    Sync config/database.php

--- config/database.php
+++ config/database.php
@@ -126,7 +126,6 @@ return [
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
-
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
